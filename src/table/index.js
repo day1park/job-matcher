0e3bb032bd704276cc63fd7a1839999d6cdf95b9
day1park <daypark2408@gmail.com>
2018-12-05 04:40:32 +1100

    style table,header, and navbar

--- src/table/index.js
+++ src/table/index.js
@@ -6,6 +6,7 @@ const Table = ({ jobData }) => {
   let jobCard;
   jobCard = jobData.map(job => {
     const { logo, name, address } = job.company;
+    const nameCaps = name.toUpperCase();
     const { jobId, title, shifts, branch, branchPhoneNumber } = job;
     var wage = `${job.wagePerHourInCents}`;
     const wagePerHour = `$${wage / 100}0/hour`;
@@ -13,28 +14,46 @@ const Table = ({ jobData }) => {
     let lastDay = shifts[shifts.length - 1].endDate;
     return (
       <div key={jobId} className="job-card">
-        <img src={logo} />
-        <h3> {title} </h3>
-        <h5>{name}</h5>
-        <h6>{wagePerHour}</h6>
-        <h6>
-          {firstDay} - {lastDay}
-        </h6>
-        <b>If you take this job you are agreeing to work ALL DAYS.</b>
-        <h6>random times</h6>
+        <div className="job-logo-info">
+          <img alt="something" src={logo} className="job-logo" />
+          <div className="job-info">
+            <label className="job-title"> {title} </label>
+            <label className="job-name">{nameCaps}</label>
+            <label className="job-wage">{wagePerHour}</label>
+
+            <label className="job-days">
+              {firstDay} - {lastDay}
+            </label>
+          </div>
+        </div>
+        <div className="job-timetable">
+          <b>If you take this job you are agreeing to work ALL DAYS.</b>
+          <ul>
+            <li>random times</li>
+            <li>random times</li>
+            <li>random times</li>
+            <li>random times</li>
+          </ul>
+        </div>
         <hr />
-        <h6>Location</h6>
-        <a href="/">
-          <u>{address}</u>
-        </a>
+        <div className="job-location">
+          <h6>Location</h6>
+          <a href="/">
+            <u>{address}</u>
+          </a>
+        </div>
         <hr />
-        <h6>Branch:</h6>
-        <h6>{branch}</h6>
-        <a href="/">
-          <u>({branchPhoneNumber}) 922-4240</u>
-        </a>
+        <div className="job-branch">
+          <h6>Branch:</h6>
+          <h6>{branch}</h6>
+          <a href="/">
+            <u>({branchPhoneNumber}) 922-4240</u>
+          </a>
+        </div>
         <hr />
-        <a href="/">NO THANKS</a> <a href="/">I'LL TAKE IT</a>
+        <div className="job-selection">
+          <a href="/">NO THANKS</a> <a href="/">I'LL TAKE IT</a>
+        </div>
       </div>
     );
   });
