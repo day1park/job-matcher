f39dc9607767df1d086725e96e9cb6cc427f122e
day1park <daypark2408@gmail.com>
2018-12-05 00:12:55 +1100

    get data and set state

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import React, { Component } from "react";
 import "./App.css";
+import getData from "./data/get-data";
 import MainNav from "./navbar/navbar";
 import Header from "./header";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      jobData: []
+    };
+  }
+  componentDidMount() {
+    const data = getData();
+    data.then(info => {
+      this.setState({
+        jobData: info
+      });
+      console.log(this.state);
+    });
+  }
   render() {
     return (
       <div className="App">
